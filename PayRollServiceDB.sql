be973ab51940f4a1e90b85958c182e919b8e51c9
VikasBenki <vikasmsbenki@gmail.com>
2022-03-25 21:19:09 +0530

    [VikasBenki] Adding/Implementing the UC2 Create Table

--- PayRollServiceDB.sql
+++ PayRollServiceDB.sql
@@ -2,3 +2,13 @@
 Create DataBase Payroll_Service
 use Payroll_Service
 select db_name() as CurrentDatabase
+
+-------------UC2-Creating the Table for the database-----
+Create Table Employee_Payroll
+(
+id int identity,
+Name varchar(50),
+salary float,
+startdate date
+);
+select *from Employee_Payroll;
